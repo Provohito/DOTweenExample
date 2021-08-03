d1adc75c11f2bc608f988772acc4ba5dde570af5
Alex Semchenkov <zigola12@list.ru>
2021-08-03 17:14:59 +0300

    Create Delay Animation,Repeating and looping the animation
    Using Delay animation with using methods SetDelay(). Using SetLoops() method to loop animation "different properties"

--- Assets/MainFolder/Scripts/MoveTo.cs
+++ Assets/MainFolder/Scripts/MoveTo.cs
@@ -9,7 +9,8 @@ namespace MainFolder
     {
         public void Start()
         {
-            transform.DOMove(new Vector3(0,5,0), 3).From();
+            //transform.DOMove(new Vector3(0,5,0), 3).SetLoops(4, LoopType.Restart);  // Анимация работает обрывками, к старту
+            transform.DOMove(new Vector3(0, 5, 0), 3).SetLoops(4, LoopType.Yoyo); // Анимация работает циклом, "Туда - обратно", если поставить -1 , то анимация бесконечно производится
         }
 
     }
