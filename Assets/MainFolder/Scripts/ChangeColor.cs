c4dd01358d32631897c1e2cbc086e1a37ab18770
Alex Semchenkov <zigola12@list.ru>
2021-08-03 17:41:02 +0300

    Work with Color
    Use DOFade and DOColor methods.

---
+++ Assets/MainFolder/Scripts/ChangeColor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace MainFolder
+{
+    public class ChangeColor : MonoBehaviour
+    {
+        private SpriteRenderer _spriteRenderer;
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            
+            _spriteRenderer.DOColor(Color.green, 2).SetLoops(-1, LoopType.Yoyo); // Ф-ция смены цвета за n-ое время
+            _spriteRenderer.DOFade(0, 0.5f).SetLoops(-1, LoopType.Yoyo); // Ф-ция затухания за n-ое время
+        }
+    }
+}
